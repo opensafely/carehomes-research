c45f84ec7e87ce953a5ba87dd9020ec20807f471
Emily Nightingale <phpuenig@lshtm.ac.uk>
2020-10-13 14:44:31 +0100

    add study def to extract HH id, size, msoa for all patients

---
+++ analysis/study_definition_coverage.py
@@ -0,0 +1,55 @@
+  
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    codelist_from_csv,
+    codelist,
+    filter_codes_by_category,
+    combine_codelists
+)
+
+## CODE LISTS
+# All codelist are held within the codelist/ folder.
+from codelists import *
+
+study = StudyDefinition(
+    # Configure the expectations framework
+    default_expectations={
+        "date": {"earliest": "2020-01-01", "latest": "today"},
+        "rate": "exponential_increase",
+        "incidence" : 0.2
+    },
+    # This line defines the study population
+    population=patients.registered_with_one_practice_between(
+        "2020-01-01", "2020-02-01"
+    ),
+
+    household_id=patients.household_as_of(
+        "2020-02-01",
+        returning="pseudo_id",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 1000, "stddev": 200},
+            "incidence": 1,
+        },
+    ),
+
+    # extract household size and MSOA
+    household_size=patients.household_as_of(
+        "2020-02-01",
+        returning="household_size",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 1},
+            "incidence": 1,
+        },
+    ),
+
+    msoa=patients.registered_practice_as_of(
+        "2020-02-01",
+        returning="msoa_code",
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"MSOA1": 0.5, "MSOA2": 0.5}},
+        },
+    ),    
+
+)
